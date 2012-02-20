af8f93a79e54f8241fc15f33393619aba6a9c3a6
AndriyK <kondratovets.andrey@gmail.com>
2012-02-21 00:40:23 +0300

    Initial commit for timer app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timer::Application.config.secret_token = '20d0505eb31e07f0228095e85a4aa6d4f6efeeb9bd6ca0cae50c2855c7810b5eb5c2da8965b1b0f42bc1f4f29ded48c2d8703be96e9402344cf826871bd347b4'
