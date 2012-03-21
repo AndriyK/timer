4e691dcbea459078eef2720ed17f5d3b38998763
AndriyK <kondratovets.andrey@gmail.com>
2012-03-22 00:02:33 +0300

    add association source with work

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -2,4 +2,10 @@ module SourcesHelper
   def source_tag? tag
     @source.tags.include?(tag) unless @source.nil?
   end
+
+  def with_work?
+    current_user.works.find(params['work_id']) if params['work_id']
+  rescue
+    nil
+  end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,5 +1,5 @@
 class Source < ActiveRecord::Base
-  attr_accessible :content, :tag_ids
+  attr_accessible :content, :tag_ids, :work_id
 
   belongs_to :user
   has_and_belongs_to_many :tags
