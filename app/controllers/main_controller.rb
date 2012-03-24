d44cc6c5abf00a91c9ec13035e9ad185f10976bc
AndriyK <kondratovets.andrey@gmail.com>
2012-03-24 23:03:30 +0300

    adding layout HP

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,15 @@
 class MainController < ApplicationController
+
+  before_filter :redirect_user, :only => [:home]
+
   def home
     @title = 'Home'
   end
 
+
+  private
+
+  def redirect_user
+    redirect_to user_path(current_user) if signed_in?
+  end
 end
