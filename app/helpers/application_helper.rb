4dbfa2494a35054adf9c5842b324afcac4df543d
AndriyK <kondratovets.andrey@gmail.com>
2012-04-16 11:12:37 +0300

    add tooltip for timeline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,14 @@ module ApplicationHelper
     'active' if action == params[:controller] + (only_controller == true ? '' : ('#'+params[:action]))
   end
 
+  def get_tooltip_text time_id
+    if time_id >= 60
+        time = time_id/60<10 ? '0' + (time_id/60).to_s : (time_id/60).to_s
+        time = time + ':'
+        time = time + (time_id%60 == 0 ? '00' : (time_id%60).to_s)
+    else
+        time = '00:' + ( time_id == 0 ? '00' : time_id ).to_s
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -48,22 +48,26 @@ class User < ActiveRecord::Base
     (user && user.salt == cookie_salt) ? user : nil
   end
 
+
+
   private
 
-  def encrypt_password
-    self.salt = make_salt if new_record?
-    self.encrypted_password = encrypt(password)
-  end
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
 
-  def encrypt(string)
-    secure_hash("#{salt}--#{string}")
-  end
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
 
-  def make_salt
-    secure_hash("#{Time.now.utc}--#{password}")
-  end
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
 
-  def secure_hash(string)
-    Digest::SHA2.hexdigest(string)
-  end
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
+
 end
