21163a7cb82af3190384bf1ffd288403f3bd5341
AndriyK <kondratovets.andrey@gmail.com>
2012-04-24 23:01:46 +0300

    add validations for work

--- app/models/work.rb
+++ app/models/work.rb
@@ -21,7 +21,10 @@ class Work < ActiveRecord::Base
   validates :description, :presence => true, :length => {:in => 1..255, :message => "Description has not correct length" }
   validates :user_id, :presence => true
 
+  validate :time_validation
+
   before_save :set_duration
+
 
   private
 
@@ -31,6 +34,30 @@ class Work < ActiveRecord::Base
       self.duration = (to - from)/60
     rescue
       0
+    end
+
+    def time_validation
+      if to <= from
+        errors.add(:to, "should be greater than from value")
+      end
+      check_intersection
+    end
+
+    def check_intersection
+      user = User.find(user_id)
+      user_works = user.works.where(:from => from.midnight .. (from.midnight + 1.day) )
+      user_works.each do |work|
+        if (from >= work.from && work.to > to)  # inside time interval
+            errors.add(:base, "Times should not intersect")
+        elsif (to > work.from && to < work.to) || ((from > work.from && from < work.to)) # start or ends in time interval
+          errors.add(:base, "Times should not intersect!")
+        elsif (from < work.from && work.to < to)
+          errors.add(:base, "Times should not overwrite another intervals!")
+        end
+      end
+    rescue
+      #errors.add(:base, "some unexpected error appear")
+      errors.add(:base, user_works.inspect)
     end
 
 end
