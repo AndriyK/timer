55b5d360b5a0b139aa4d574f364dbaaa0dfff44a
AndriyK <kondratovets.andrey@gmail.com>
2012-07-16 21:33:01 +0300

    improve categories representation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,11 @@ class CategoriesController < ApplicationController
   before_filter :authorized_user, :only => [:edit, :update, :destroy]
   before_filter :prepare_pcategory, :only => [:create, :update]
   before_filter :clean_parent, :only => [:destroy]
+  before_filter :set_user, :only => [:show]
+  before_filter :prepare_categories_catalogue, :only => [:show]
 
   def show
-    @user = current_user
-    @categories = @user.categories.order("name")
+    @categories = get_categories_hash
     @category = Category.new
     @title = @user.name + ' categories'
   end
@@ -54,4 +55,31 @@ class CategoriesController < ApplicationController
     def clean_parent
       current_user.categories.update_all("pcategory=0", "pcategory="+@category.id.to_s)
     end
+
+    def prepare_categories_catalogue
+      @catalogue = {}
+      set_categories_for_catalogue(0,0)
+    end
+
+    def set_categories_for_catalogue(pcat, level)
+      categories = @user.categories.where(:pcategory=>pcat).order("pcategory,name")
+      categories.each do |category|
+        @catalogue[category.id] = level
+        set_categories_for_catalogue(category.id, level+1)
+      end
+    end
+
+    def get_categories_hash
+      all_categories = @user.categories
+      categories_hash = {}
+      all_categories.each do |category|
+        categories_hash[category.id] = category
+      end
+      categories_hash
+    end
+
+    def set_user
+      @user = current_user
+    end
+
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,13 +1,5 @@
 module CategoriesHelper
 
-  def get_category_name id
-    unless id.nil?
-      if (cat = Category.find_by_id( id ) )
-        cat.name
-      end
-    end
-  end
-
   def get_possible_parent_categories category
     possible_categories = current_user.categories
     if category.id then
