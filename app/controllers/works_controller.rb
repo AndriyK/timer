b4dfe47fa32dbd7c675e3c709b51a36d5a375ffa
AndriyK <kondratovets.andrey@gmail.com>
2012-07-17 22:58:21 +0300

    improve categories representation on work page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -15,6 +15,7 @@ class WorksController < ApplicationController
   before_filter :save_custom_date, :only => [:show, :week, :month]
   before_filter :user_works, :only => [:show, :edit, :new]
   before_filter :correct_midnight_time, :only => [:create, :update]
+  before_filter :prepare_categories_catalogue, :only => [:show, :edit]
 
   def show
     @day = true
@@ -335,6 +336,29 @@ class WorksController < ApplicationController
         params[:work]['to(4i)'] = '23'
         params[:work]['to(5i)'] = '59'
       end
+    end
+
+    def prepare_categories_catalogue
+      @categories = get_categories_hash
+      @catalogue = {}
+      set_categories_for_catalogue(0,0)
+    end
+
+    def set_categories_for_catalogue(pcat, level)
+      categories = current_user.categories.where(:pcategory=>pcat).order("pcategory,name")
+      categories.each do |category|
+        @catalogue[category.id] = level
+        set_categories_for_catalogue(category.id, level+1)
+      end
+    end
+
+    def get_categories_hash
+      all_categories = current_user.categories
+      categories_hash = {}
+      all_categories.each do |category|
+        categories_hash[category.id] = category
+      end
+      categories_hash
     end
 
 end
